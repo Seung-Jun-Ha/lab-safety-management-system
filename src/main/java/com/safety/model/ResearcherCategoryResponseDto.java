e03d108d1699501e4a80b56fd0d4005d388f1562
Lee Chang-min <war10303455@gmail.com>
2026-06-04 13:51:28 +0900

    feat: 담당 유스케이스 구현 및 프로젝트 인프라 정상화

--- src/main/java/com/safety/model/ResearcherCategoryResponseDto.java
+++ src/main/java/com/safety/model/ResearcherCategoryResponseDto.java
@@ -1,4 +1,15 @@
 package com.safety.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ResearcherCategoryResponseDto {
+    private String categoryId;
+    private String categoryName;
+    private String description;
+    private int researcherCount;
 }
